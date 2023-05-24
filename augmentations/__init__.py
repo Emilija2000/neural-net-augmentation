5a219d36599972c4d1b16d3b91d40a09f1bf27c1
Emilija2000 <emilidjordjevic@gmail.com>
2023-05-24 14:28:44 +0100

    full permutation augmentations for conv and linear layers

---
+++ augmentations/__init__.py
@@ -0,0 +1,3 @@
+from permutation_augmentation import permute_batch,permute_checkpoint,perform_single_permutation
+from augment_utils import augment, augment_batch
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='neural-net-augmentations',
+      version='1.0.0',
+      packages=["augmentations"],
+      install_requires=[
+        "jax",
+        "numpy",
+        ],
+    )
